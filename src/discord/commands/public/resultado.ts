53c14171c2c67e0768b0f95f0cccf62267743ee6
Kayo Oliveira <g.kayooliveira123@gmail.com>
2025-01-14 10:29:35 -0300

    Remove 'aprovarporte' command and add 'resultado' command for course and weapon permit results

---
+++ src/discord/commands/public/resultado.ts
@@ -0,0 +1,138 @@
+import { createCommand } from "#base";
+import {
+  ApplicationCommandType,
+  ApplicationCommandOptionType,
+} from "discord.js";
+import { curso } from "functions/resultado/curso.js";
+import { porte } from "functions/resultado/porte.js";
+
+
+createCommand({
+  name: "resultado",
+  description: "Enviar resultado de um curso ou exame",
+  type: ApplicationCommandType.ChatInput,
+  options: [
+    {
+      name: "porte",
+      type: ApplicationCommandOptionType.Subcommand,
+      description: "Resultado de um porte de armas",
+      options: [
+        {
+          name: "nome",
+          description: "Nome do cidadão",
+          type: ApplicationCommandOptionType.String,
+          required: true,
+        },
+        {
+          name: "passaporte",
+          description: "Número do passaporte",
+          type: ApplicationCommandOptionType.String,
+          required: true,
+        },
+        {
+          name: "registro",
+          description: "Número do registro",
+          type: ApplicationCommandOptionType.String,
+          required: true,
+        },
+        {
+          name: "numero",
+          description: "Número do porte",
+          type: ApplicationCommandOptionType.String,
+          required: true,
+        },
+        {
+          name: "necessidade",
+          description: "Necessidade do porte",
+          type: ApplicationCommandOptionType.String,
+          required: true,
+        },
+        {
+          name: "profissao",
+          description: "Profissão do cidadão",
+          type: ApplicationCommandOptionType.String,
+          required: true,
+        },
+        {
+          name: "status",
+          description: "Status do porte",
+          type: ApplicationCommandOptionType.String,
+          required: true,
+          choices: [
+            {
+              name: "Aprovado ✅",
+              value: "APROVADO",
+            },
+            {
+              name: "Reprovado ❌",
+              value: "REPROVADO",
+            },
+          ],
+        },
+        {
+          name: "arquivo",
+          description: "Arquivo do porte",
+          type: ApplicationCommandOptionType.Attachment,
+          required: false,
+        },
+      ],
+    },
+    {
+        name: "curso",
+        type: ApplicationCommandOptionType.Subcommand,
+        description: "Resultado de um curso",
+        options: [
+            {
+                name: 'usuario',
+                description: 'Usuário que realizou o curso',
+                type: ApplicationCommandOptionType.User,
+            },
+            {
+                name: 'cargo',
+                description: 'Cargo atual do usuário',
+                type: ApplicationCommandOptionType.Role,
+            },
+            {
+                name: 'curso',
+                description: 'Curso realizado',
+                type: ApplicationCommandOptionType.Role,
+            },
+            {
+                name: 'status',
+                description: 'Status do curso',
+                type: ApplicationCommandOptionType.String,
+                required: true,
+                choices: [
+                    {
+                        name: 'Aprovado ✅',
+                        value: 'APROVADO',
+                    },
+                    {
+                        name: 'Reprovado ❌',
+                        value: 'REPROVADO',
+                    }
+                ]
+            }
+        ]
+    }
+  ],
+  async run(interaction) {
+    const { guild, user, options } = interaction;
+    
+    if (!guild) {
+      return interaction.reply({
+        content: "Este comando só pode ser executado em um servidor",
+        ephemeral: true,
+      });
+    }
+
+    switch (options.getSubcommand()){
+        case 'porte':
+            return interaction.reply(porte(options,user))
+        case 'curso':
+            return interaction.reply(curso(options,user))
+        default: 
+            return interaction.reply({content: 'Comando inválido', ephemeral: true})
+    }
+  },
+});
---
+++ src/functions/resultado/porte.ts
@@ -0,0 +1,45 @@
+import { createEmbed } from "@magicyan/discord";
+import { CommandInteractionOptionResolver,  InteractionReplyOptions, User } from "discord.js";
+
+export function porte<R>(options:Omit<CommandInteractionOptionResolver<'cached'>, 'getMessage' | 'getFocused'> ,user:User):R {
+
+  const nome = options.getString("nome", true);
+  const passaporte = options.getString("passaporte", true);
+  const registro = options.getString("registro", true);
+  const numero = options.getString("numero", true);
+  const necessidade = options.getString("necessidade", true);
+  const profissao = options.getString("profissao", true);
+  const arquivo = options.getAttachment("arquivo");
+
+  const embed = createEmbed({
+    title: "RESULTADO DE EXAME PSICOTÉCNICO PARA PORTE DE ARMA",
+    image: {
+      url: "https://i.imgur.com/IzCwSPI.png",
+    },
+    description: `
+          Nome: ${nome}
+          Passaporte: ${passaporte}
+          Registro: ${registro}
+          Número: ${numero}
+          Necessidade: ${necessidade}
+          Profissão: ${profissao}
+          Solicitante: <@${user.id}>
+
+          STATUS: APROVADO ✅
+          `,
+    color: "#00ff00",
+  });
+
+  if (arquivo) {
+    embed.addFields([
+      {
+        name: `Certificado do Exame Psicotécnico ${nome}`,
+        value: `[Baixar](${arquivo.url})`,
+      },
+    ]);
+  }
+
+  return ({
+    embeds: [embed],
+  } satisfies InteractionReplyOptions) as R
+}
